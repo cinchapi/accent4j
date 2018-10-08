7873e14661eba1d2e967733dae70cfc8a00c24a7
Jeff Nelson <jeff@cinchapi.com>
2018-10-08 12:22:28 -0400

    Revert "Revert "fix a bug in how AnyMaps#merge* handles null values""
    This reverts commit b8fac1f384d29fa456ce42ebfda71f637c397fb8.

--- src/main/java/com/cinchapi/common/collect/AnyMaps.java
+++ src/main/java/com/cinchapi/common/collect/AnyMaps.java
@@ -192,7 +192,18 @@ public final class AnyMaps {
             Map<String, Object> from,
             BiFunction<Object, Object, Object> strategy) {
         from.forEach((key, value) -> {
-            into.merge(key, value, strategy);
+            boolean merged = false;
+            while (!merged) {
+                if(into.containsKey(key)) {
+                    Object ours = into.get(key);
+                    merged = into.replace(key, ours,
+                            strategy.apply(ours, value));
+                }
+                else {
+                    merged = into.putIfAbsent(key,
+                            strategy.apply(null, value)) == null;
+                }
+            }
         });
     }
 
