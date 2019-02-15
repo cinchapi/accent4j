51a28f0f35cd86821230db88f99ef5c162a2a884
Jeff Nelson <jeff@cinchapi.com>
2019-02-15 12:11:43 -0500

    add vararg param to isWithinQuotes to force ignore certain characters when considering

--- src/main/java/com/cinchapi/common/base/AnyStrings.java
+++ src/main/java/com/cinchapi/common/base/AnyStrings.java
@@ -542,10 +542,14 @@ public class AnyStrings {
      * double quotes.
      * 
      * @param string
+     * @param forceTreatAsNotQuoteChar a list of characters may be
+     *            technically quotes but should not be considered when checking
+     *            if {@code string} is within quotes
      * @return {@code true} if the string is between quotes
      */
-    public static boolean isWithinQuotes(String string) {
-        string = replaceUnicodeConfusables(string);
+    public static boolean isWithinQuotes(String string,
+            Character... forceTreatAsNotQuoteChar) {
+        string = replaceUnicodeConfusables(string, forceTreatAsNotQuoteChar);
         if(string.length() > 2) {
             char first = string.charAt(0);
             if(first == '"' || first == '\'') {
